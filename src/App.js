d91ee2466a91fccab39761d3ab993ecb808568d2
claaraamaria <clrstancu@gmail.com>
2020-08-28 16:32:20 +0300

    added react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      hello
-    </div>
+    <Switch>
+      <Route exact path="/">
+        This is homezpage
+      </Route>
+
+      <Route exact path="/starred">
+        This is starred
+      </Route>
+
+      <Route>
+        This is 404 oage      
+      </Route>
+    </Switch>
   );
 }
 
